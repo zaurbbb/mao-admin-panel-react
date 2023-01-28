c28aa04506c8bd88165f62341701f524fdf505cd
zaurbbb <zaurberdibekov@mail.ru>
2023-01-28 17:10:31 +0600

    add 7-1 pattern to scss
    Took 8 minutes

--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,7 @@ import { ThemeProvider } from "@mui/material";
 import { theme } from "./themes/theme";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import "./styles/main.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
